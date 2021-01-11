5ab19f861d67bbc324f523dc22a8e7ff0173e731
kastenbauer27 <71998793+kastenbauer27@users.noreply.github.com>
2021-01-10 18:48:43 -0600

    Initial commit

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Footer extends Component {
+    state = {  }
+    render() { 
+        return ( 
+            <footer>
+                <h3>Have fun!</h3>
+                <p>Don't forget to mind your Ps and Qs!</p>
+            </footer>
+        );
+    }
+}
+ 
+export default Footer;
