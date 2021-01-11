eadda363b3b1e09e72610975a66235d6c99c47fb
kastenbauer27 <pkast2016@comcast.net>
2021-01-10 19:03:12 -0600

    created timer functionality with alert when time is at zero

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,58 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
-import Header from '../Header/Header';
-import Footer from '../Footer/Footer';
-import GuestList from '../GuestList/GuestList';
-import DinnerSupplies from '../DinnerSupplies/DinnerSupplies';
-import GuestForm from '../GuestForm/GuestForm';
+import Timer from '../Timer/Timer';
+
 
 class App extends Component {
-  state = {
-    guestList: [],
-    newGuest: {
-      name: '',
-      kidsMeal: 'no',
-    },
-  }
-
-  handleChangeFor = (propertyName) => (event) => {
-    this.setState({
-      newGuest: {
-        ...this.state.newGuest,
-        [propertyName]: event.target.value,
-      }
-    });
-  }
-
-  handleSubmit = (event) => {
-    event.preventDefault();
-    if (this.state.newGuest.name) {
-      this.setState({
-        guestList: [...this.state.guestList, this.state.newGuest],
-        newGuest: {
-          name: '',
-          kidsMeal: 'no',
-        },
-      });
-    } else {
-      alert('The new guest needs a name!');
-    }
-  }
-
+ 
   render() {
     return (
       <div className="App">
-        <Header />
-        <h2>Party Leader</h2>
-        {this.state.guestList[0] && <h3>{this.state.guestList[0].name}</h3>}
-        <GuestForm 
-          newGuest={this.state.newGuest}
-          handleChangeFor={this.handleChangeFor}
-          handleSubmit={this.handleSubmit}
-        />
-        <GuestList guests={this.state.guestList}/>
-        <DinnerSupplies count={this.state.guestList.length}/>
-        <Footer />
+        <Timer />
       </div>
     );
   }
